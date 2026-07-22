8712fc62466ca3893547e2454560e2c1292bb1af
Mahakal ketan Hasmukhbhai <ketanmahakal123@gmail.com>
2026-07-23 00:22:54 +0530

    MY Project backend step and  PostgreSQL database connection

---
+++ app/core/config.py
@@ -0,0 +1,15 @@
+import os
+
+class Settings:
+    PROJECT_NAME: str = "Car Dealership Inventory"
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER", "postgres")
+    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD", "password")
+    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
+    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", "5432")
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB", "car_inventory")
+    
+    @property
+    def DATABASE_URL(self) -> str:
+        return f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_SERVER}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+
+settings = Settings()
